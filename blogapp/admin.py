2a4558f1eff9494f973e3023642f924ec23ee2c4
grolarkim <audwnswmf@naver.com>
2022-06-14 22:04:04 +0900

    create_comment_model

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from blogapp.models import Category, Article
+from blogapp.models import Category, Article, Comment
 
-# Register your models here.
-admin.site.register(Article)
-admin.site.register(Category)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("article", "author", "content")
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blogapp/models.py
+++ blogapp/models.py
@@ -3,8 +3,11 @@ from userapp.models import User
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(verbose_name="카테고리", max_length=100)
+    name = models.CharField(verbose_name="카테고리", max_length=100, unique=True)
     description = models.TextField(verbose_name="설명")
+    
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
@@ -12,3 +15,19 @@ class Article(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     title = models.CharField(verbose_name="제목", max_length=100)
     content = models.TextField(verbose_name="내용")
+    created_at = models.DateTimeField(verbose_name="생성시각", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="수정시각", auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(to=Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name="내용")
+    created_at = models.DateTimeField(verbose_name="생성시각", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="수정시각", auto_now=True)
+    
+    def __str__(self):
+        return f"{self.author} : {self.content}"
